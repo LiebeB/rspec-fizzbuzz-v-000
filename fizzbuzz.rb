707074f00fe2852ed86beb5198239b9e00a9bdf7
Liebe Brand <liebebrand@gmail.com>
2018-07-31 08:06:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,7 +9,9 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
   elsif int % 5 == 0 # if the number int is divisible by 5
     "Buzz" # Go buzz
-  else int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 5 and 3
+  elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 5 and 3
     "FizzBuzz" # Go fizzbuzz
+  else 
+    int
   end
 end
