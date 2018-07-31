8fbb87d42bceb7d2eb141e7bb0f414030feb059b
Liebe Brand <liebebrand@gmail.com>
2018-07-31 08:00:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,11 +7,5 @@ end
 def fizzbuzz(int)
   if i % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif i % 5 == 0 # if the number int is divisible by 5
-    "Buzz" # Go Buzz
-  elsif i % 3 == 0 and int % 5 == 0 # if the number int is divisible by 15
-    "FizzBuzz" # Go FizzBuzz
-  else 
-    "i"  # no nothing
   end
 end
